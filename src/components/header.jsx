43da0bb48e17f8e2dc5dca1ad3d1a53482adeada
MasmelaDev <miguelmasmeladev@gmail.com>
2024-01-17 00:52:13 -0500

    Se crea componente Searcher

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,13 @@
+import Searcher from "./searcher";
+
 export default function Header() {
   return (
-    <header></header>
-  )
+    <header >
+      <h2 className="flex gap-1 text-2xl">
+        Right now in
+        <Searcher />
+        it`s <span>clear</span>
+      </h2>
+    </header>
+  );
 }
--- src/components/searcher.jsx
+++ src/components/searcher.jsx
@@ -1,5 +1,72 @@
+import { useEffect, useState, useRef } from "react";
+import Typewriter from "typewriter-effect";
+import { useSearchParams } from "react-router-dom";
+
 export default function Searcher() {
+  const [writing, setWriting] = useState(false);
+  const searcherRef = useRef(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleClick = async () => {
+    setWriting(true);
+  };
+
+  useEffect(() => {
+    const searcherElement = searcherRef.current;
+    if (writing) {
+      searcherElement.focus();
+    }
+
+    const handleClickOutside = (event) => {
+      if (
+        searcherElement &&
+        !searcherElement.contains(event.target) &&
+        event.target.tagName.toLowerCase() !== "span"
+      ) {
+        setSearchParams({ city: searcherElement.value });
+        setWriting(false);
+      }
+    };
+
+    const handleEnter = (event) => {
+      if (event.key === "Enter") {
+        setSearchParams({ city: searcherElement.value });
+        setWriting(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("keydown", handleEnter);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("keydown", handleEnter);
+    };
+  }, [writing]);
+  
   return (
-    <div>searcher</div>
-  )
+    <>
+      {writing ? (
+        <input
+          ref={searcherRef}
+          type="text"
+          className="bg-transparent focus:outline-none w-40 focus:border-none px-5 searcher text-center"
+          placeholder={searchParams.get("city") || "New York"}
+          id="searcher"
+        />
+      ) : (
+        <span
+          className="cursor-pointer px-5 w-40 searcher text-center"
+          onClick={handleClick}
+        >
+          <Typewriter
+            component={"span"}
+            options={{
+              strings: [searchParams.get("city") || "New York"],
+              autoStart: true,
+              loop: true
+            }}
+          />
+        </span>
+      )}
+    </>
+  );
 }
